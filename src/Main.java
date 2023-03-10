79a7fe8400f4d685e617977221dcb3539aee2c53
luca_ <luca_129@hotmail.de>
2023-03-10 16:06:46 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public MainFrame(){
+    setTitle(//String title of Mainframe);
+    setSize(//x, y size);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setVisible(true);
+    setContentPane(//name of Mainframe);
+    }
